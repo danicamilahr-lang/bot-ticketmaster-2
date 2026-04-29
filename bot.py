855ef2af6a6683ff66d272d28a76011d8e254a1d
danicamilahr-lang <danicamilahr@gmail.com>
2026-04-28 19:57:08 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,12 +21,9 @@ def enviar_telegram(msg):
 def iniciar_driver():
     options = Options()
 
-    options.binary_location = "/usr/bin/chromium"
-
-    options.add_argument("--headless=new")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
 
     driver = webdriver.Chrome(options=options)
 
